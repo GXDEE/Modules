53f3a866932a688dff429f3231d17786ef55647f
GXDEE <Van.anime.maa@icloud.com>
2025-12-11 10:02:20 +0000

    small fix

--- Gift.py
+++ Gift.py
@@ -1,4 +1,4 @@
-__version__ = (1,0)
+__version__ = (1,0,0)
 
 # module: Gift
 # meta developer: GXDEE.t.me
--- Who.py
+++ Who.py
@@ -1,4 +1,4 @@
-version = (1, 0)
+version = (1,0,0)
 
 # module: Who
 # meta developer: GXDEE.t.me
